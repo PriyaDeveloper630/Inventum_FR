fe1a63820e2ca341b2116b63ee7fe300cfa48783
himanshi4902 <sharmahimanshi0094@gmail.com>
2023-02-12 18:15:59 +0530

    login page

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -7,6 +7,7 @@ import SignUp from "./pages/Signup";
 import Profile from "./pages/Profile";
 import Network from "./pages/Network";
 import Landingpage from "./pages/landingpage/landingpage.jsx";
+import Login from "./pages/login/loginpage.jsx";
 import './App.css';
 
 function App() {
@@ -15,6 +16,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/landingpage" element={<Landingpage/>}/>
+        <Route path="/login" element={<Login/>}/>
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/dashboard" element={<Dashboard />} />
